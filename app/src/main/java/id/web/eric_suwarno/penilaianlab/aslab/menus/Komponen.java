ae3da42cb5aa5ff7943abb54ef143b33cb20cc6b
Eric Suwarno <121402071.es@gmail.com>
2016-01-21 07:49:10 +0700

    Perbaikan besar-besaran.

---
+++ app/src/main/java/id/web/eric_suwarno/penilaianlab/aslab/menus/Komponen.java
@@ -0,0 +1,31 @@
+package id.web.eric_suwarno.penilaianlab.aslab.menus;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.web.eric_suwarno.penilaianlab.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Komponen extends Fragment {
+
+
+    public Komponen() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.aslab_komponen, container, false);
+    }
+
+
+}
