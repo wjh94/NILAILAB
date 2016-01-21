c037b1d8c95d7f6f36d541e7b3bb974aa41bd408
Eric Suwarno <121402071.es@gmail.com>
2016-01-21 11:25:20 +0700

    Perbaikan jenis fragmen.

--- app/src/main/java/id/web/eric_suwarno/penilaianlab/aslab/Main.java
+++ app/src/main/java/id/web/eric_suwarno/penilaianlab/aslab/Main.java
@@ -1,15 +1,103 @@
 package id.web.eric_suwarno.penilaianlab.aslab;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import id.web.eric_suwarno.penilaianlab.R;
+import id.web.eric_suwarno.penilaianlab.aslab.menus.Home;
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout drawerLayout;
+    private ListView navList;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main_aslab);
+        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        navList = (ListView) findViewById(R.id.navlist);
+        ArrayList<String> navArray = new ArrayList<String>();
+        String[] arr = getResources().getStringArray(R.array.menu_aslab);
+        for(int i = 0; i < arr.length; i++){
+            navArray.add(arr[i]);
+        }
+        navList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, navArray);
+
+        navList.setAdapter(arrayAdapter);
+        navList.setOnItemClickListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null) {
+            throw new AssertionError();
+        } else {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            fragmentManager = getSupportFragmentManager();
+
+            loadSelection(0);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
+
+    private void loadSelection(int position) {
+        navList.setItemChecked(position, true);
+
+        switch(position){
+            case 0:
+                Home home = new Home();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentholder, home);
+                fragmentTransaction.commit();
+                break;
+
+            case 5:
+                new AlertDialog.Builder(this)
+                        .setTitle("Konfirmasi")
+                        .setMessage("Apakah Anda yakin ingin keluar?")
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Tidak", null)
+                        .show();
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
